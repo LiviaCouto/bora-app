52d69ed866e9d1e656794d6cffd6d5786f406442
Lívia Couto <84749105+LiviaCouto@users.noreply.github.com>
2026-07-25 20:31:27 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // BORA — Service Worker (cache offline básico)
 // ============================================================
 
-const CACHE_NOME = 'bora-v1';
+const CACHE_NOME = 'bora-v2';
 const ARQUIVOS_PARA_CACHE = [
   './',
   './index.html',
